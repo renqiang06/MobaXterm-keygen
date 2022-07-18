79e7f9ab46cf9fd75ebd14b0e62d6a6bedb7648f
renqiang06 <renqiang06@126.com>
2022-07-18 14:57:12 +0800

    update

--- MobaXterm-Keygen.py
+++ MobaXterm-Keygen.py
@@ -1,13 +1,20 @@
 #/usr/bin/env python3
 '''
-Author: Double Sine
-License: GPLv3
+Author: renqiang06
 '''
-import os, sys, zipfile
+import os, sys, zipfile, random, socket, time
 
+UserNameLength = 5
 VariantBase64Table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
 VariantBase64Dict = { i : VariantBase64Table[i] for i in range(len(VariantBase64Table)) }
-VariantBase64ReverseDict = { VariantBase64Table[i] : i for i in range(len(VariantBase64Table)) }
+RandomUserName = ''.join(random.sample(VariantBase64Table[:-3], UserNameLength))
+hostname = socket.gethostname()
+
+def write_logfile(text):
+    logfile = open('log_{0}.txt'.format(hostname[-6:]), 'a')
+    logfile.write(time.strftime(
+        "%Y-%m-%d %H:%M:%S", time.localtime()) + ':' + hostname + str(text) + '\n')
+    logfile.close()
 
 def VariantBase64Encode(bs : bytes):
     result = b''
@@ -37,33 +44,6 @@ def VariantBase64Encode(bs : bytes):
         result += block.encode()
         return result
 
-def VariantBase64Decode(s : str):
-    result = b''
-    blocks_count, left_bytes = divmod(len(s), 4)
-
-    for i in range(blocks_count):
-        block = VariantBase64ReverseDict[s[4 * i]]
-        block += VariantBase64ReverseDict[s[4 * i + 1]] << 6
-        block += VariantBase64ReverseDict[s[4 * i + 2]] << 12
-        block += VariantBase64ReverseDict[s[4 * i + 3]] << 18
-        result += block.to_bytes(3, 'little')
-
-    if left_bytes == 0:
-        return result
-    elif left_bytes == 2:
-        block = VariantBase64ReverseDict[s[4 * blocks_count]]
-        block += VariantBase64ReverseDict[s[4 * blocks_count + 1]] << 6
-        result += block.to_bytes(1, 'little')
-        return result
-    elif left_bytes == 3:
-        block = VariantBase64ReverseDict[s[4 * blocks_count]]
-        block += VariantBase64ReverseDict[s[4 * blocks_count + 1]] << 6
-        block += VariantBase64ReverseDict[s[4 * blocks_count + 2]] << 12
-        result += block.to_bytes(2, 'little')
-        return result
-    else:
-        raise ValueError('Invalid encoding.')
-
 def EncryptBytes(key : int, bs : bytes):
     result = bytearray()
     for i in range(len(bs)):
@@ -71,19 +51,12 @@ def EncryptBytes(key : int, bs : bytes):
         key = result[-1] & key | 0x482D
     return bytes(result)
 
-def DecryptBytes(key : int, bs : bytes):
-    result = bytearray()
-    for i in range(len(bs)):
-        result.append(bs[i] ^ ((key >> 8) & 0xff))
-        key = bs[i] & key | 0x482D
-    return bytes(result)
-
 class LicenseType:
     Professional = 1
     Educational = 3
     Persional = 4
 
-def GenerateLicense(Type : LicenseType, Count : int, UserName : str, MajorVersion : int, MinorVersion):
+def GenerateLicense(Type : LicenseType, Count : int, UserName : str = RandomUserName, MajorVersion : int = 21, MinorVersion : int = 2):
     assert(Count >= 0)
     LicenseString = '%d#%s|%d%d#%d#%d3%d6%d#%d#%d#%d#' % (Type, 
                                                           UserName, MajorVersion, MinorVersion, 
@@ -92,35 +65,38 @@ def GenerateLicense(Type : LicenseType, Count : int, UserName : str, MajorVersio
                                                           0,    # Unknown
                                                           0,    # No Games flag. 0 means "NoGames = false". But it does not work.
                                                           0)    # No Plugins flag. 0 means "NoPlugins = false". But it does not work.
+    print(LicenseString) # 打印License字符串
     EncodedLicenseString = VariantBase64Encode(EncryptBytes(0x787, LicenseString.encode())).decode()
+    print(EncodedLicenseString)
     with zipfile.ZipFile('Custom.mxtpro', 'w') as f:
         f.writestr('Pro.key', data = EncodedLicenseString)
 
 def help():
-    print('Usage:')
-    print('    MobaXterm-Keygen.py <UserName> <Version>')
-    print()
-    print('    <UserName>:      The Name licensed to')
-    print('    <Version>:       The Version of MobaXterm')
-    print('                     Example:    10.9')
-    print()
+    print(f'Usage:\n    MobaXterm-Keygen.py <UserName默认:随机5位字符> <Version默认:21.2>\n')
+    print('    <UserName>:      eg:Mrren')
+    print('    <Version>:       eg:21.2')
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        help()
-        exit(0)
-    else:
+    help()
+    UserName = ''
+    MajorVersion, MinorVersion = 0, 0
+    try:
+        UserName = sys.argv[1]
         MajorVersion, MinorVersion = sys.argv[2].split('.')[0:2]
         MajorVersion = int(MajorVersion)
         MinorVersion = int(MinorVersion)
+    except:
+        pass
+    try:
         GenerateLicense(LicenseType.Professional, 
                         1,
-                        sys.argv[1], 
+                        UserName,
                         MajorVersion, 
                         MinorVersion)
-        print('[*] Success!')
-        print('[*] File generated: %s' % os.path.join(os.getcwd(), 'Custom.mxtpro'))
+        print(f'[*] Success!\n[*] File generated: %s' % os.path.join(os.getcwd(), 'Custom.mxtpro'))
         print('[*] Please move or copy the newly-generated file to MobaXterm\'s installation path.')
-        print()
+    except Exception as e:
+        print(e)
+        write_logfile(e+', License获取失败!')
 else:
     print('[*] ERROR: Please run this script directly')
